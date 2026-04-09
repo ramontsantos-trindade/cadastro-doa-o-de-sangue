16b2f61df7fb00a6a8db9ac76aacfe6a75d0203c
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-04-09 09:57:50 -0300

    add - html e um pouco de java

---
+++ script.js
@@ -0,0 +1,6 @@
+const listaDeDoadores=[]
+
+const form = document.getElementById('formDoador');
+const listaExibicao = document.getElementById('listaDoeadores');
+
+form.addEventListener('submit', function(event))
