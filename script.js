3051cf03bd4a3d16aa183363cc586773fe3b637b
ramontsantos-trindade <ramon_t_santos@estudante.sesisenai.org.br>
2026-04-09 12:24:42 -0300

    add - finalização

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
-const listaDeDoadores=[]
+const listaDeDoadores = []
 
 const form = document.getElementById('formDoador');
 const listaExibicao = document.getElementById('listaDoeadores');
 
-form.addEventListener('submit', function(event))
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que a página recarregue
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const idade = parseInt(document.getElementById('idade').value);
+    const peso = parseFloat(document.getElementById('peso').value);
+    const tipoSanguineo = document.getElementById('tipoSanguineo').value;
+    const telefone = document.getElementById('telefone').value;
+    const cidade = document.getElementById('cidade').value;
+    const estado = document.getElementById('estado').value;
+
+    if (nome.trim().split(" ").length < 2) {
+        alert("Por favor, insira seu nome completo (nome e sobrenome).");
+        return;
+    }
+
+    if (idade < 16)
+        alert("A idade minima para cadastro é 16 anos.");
+
+    if (peso < 50) {
+        alert("O peso minimo para doação é 50kg");
+    }
+
+    if (/^\d+$/.test(telefone)) {
+        alert("O telefone deve conter apenas numeros")
+        return;
+    }
+
+    const doador = {
+        nome: nome,
+        email: email,
+        idade: idade,
+        peso: peso,
+        tipoSanguineo: tipoSanguineo,
+        telefone: telefone,
+        cidade: cidade,
+        estado: estado,
+    };
+
+    listaDeDoadores.push(doador);
+
+    console.log("Novo doador cadastrado", doador)
+    console.log("Lista completa de doadores", listaDoadores)
+
+    const li = document.createElement('li')
+    li.textContent = `${doador.nome} | Tipo: ${doador.tipoSanguineo} | ${doador.cidade}-${doador.estado}`;
+    listaExibicao.appendChild(li);
+
+    form.reset()
+    alert("Cadastro realizado com sucesso!");
+
+
+});
